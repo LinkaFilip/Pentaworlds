31461d658f0c020cc21e6925144d4f66775948ad
LinkaFilip <filip.linka@email.cz>
2025-06-02 20:26:25 +0000

    changes

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://<user>:<password>@<host>/<db_name>"  # nahraď svými daty
+SQLALCHEMY_DATABASE_URL = "postgresql://penta_worlds_database_user:tlrQsqkboG4qHcixoBAIAOQlzYtLpMfx@dpg-d0v0gammcj7s73cqrnm0-a/penta_worlds_database"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
