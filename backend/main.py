5427795854528e1a687afb467093bd5749146c00
LinkaFilip <filip.linka@email.cz>
2025-06-04 17:51:33 +0000

    changes

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://pentaworlds.com"]
+    allow_origins=["https://pentaworlds.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
