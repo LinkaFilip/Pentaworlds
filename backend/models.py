c429045cc6a60925ed4dc1420cc222d25485fa36
LinkaFilip <filip.linka@email.cz>
2025-06-05 16:29:51 +0000

    changes

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,9 @@
+import secrets
 from sqlalchemy import Column, Integer, String
 from database import Base
+
+def generate_url_hash():
+    return secrets.token_hex(12)
 
 class User(Base):
     __tablename__ = "users"
@@ -7,6 +11,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    coins = Column(Integer, default=0) 
-    url_hash = Column(String, unique=True, index=True, default=lambda: secrets.token_hex(12))
+    coins = Column(Integer, default=0)
+    url_hash = Column(String, unique=True, index=True, default=generate_url_hash)
+
 print("Everything is good from models.py")
