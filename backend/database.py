e814ca758fb4e55c28cd990df6ef349afd7579c7
LinkaFilip <filip.linka@email.cz>
2025-06-07 17:47:26 +0000

    changes

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,11 @@
+import os
 from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -16,3 +16,4 @@ def get_db():
         yield db
     finally:
         db.close()
+print(f"Using database URL: {SQLALCHEMY_DATABASE_URL}")
